4fb422c68228f91cfbcf8e6b442193149532fdfa
Jordan <jordan_chen@innodisk.com>
2023-07-10 11:17:54 +0800

    fix get all device error

--- src/store/devices.js
+++ src/store/devices.js
@@ -41,7 +41,7 @@ const devicesSlice = createSlice({
                     //return updateTemperatureInfo(state,action.meta.arg,action.payload.data);
                 }else if (action.payload.status_code===500){
                     
-                    state.error=JSON.stringify(action.payload)
+                    state.error= action.payload.message;
                     state.status = 'error';
                 
                 }else{
